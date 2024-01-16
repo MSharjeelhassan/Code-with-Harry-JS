5589bba70bc6d0eab6ef67187381c869d57aaf85
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2024-01-16 17:54:56 +0500

    Code with Harry practise at home

---
+++ app.js
@@ -0,0 +1,19 @@
+// Quiz Questions in Lecture
+
+
+
+
+// Class Quiz
+
+let a =10;
+
+console.log("++a =", ++a); 
+console.log("a++ =", a++); 
+console.log("--a =", --a);
+console.log("a-- =", a--);
+console.log("++a =",++a);
+console.log("a =", a);
+console.log("a-- =", a--);
+
+// print 11, 11,11,11,11,11,11
+// value 11, 12, 11, 10, 11, 11, 10
